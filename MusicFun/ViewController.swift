29818ba83dfc050a79b89fe45092847da338968a
Muhammad Amjad <amjidrafique786@gmail.com>
2019-03-30 15:21:03 +0500

    Initializing the MusicFun Application

---
+++ MusicFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicFun
+//
+//  Created by Muhammad Amjad on 30/03/2019.
+//  Copyright © 2019 Innvotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
